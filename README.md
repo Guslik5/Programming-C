fe0dc5d998d19d718d31d899e4bb94efefa42581
Dmitriy <guslik2003@mail.ru>
2023-04-12 14:04:30 +0300

    add ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Programming-C
+<details>
+
+<summary>Лабораторная работа 1</summary>
+<img srs="lab1.jpg">
+
+<summary>Лабораторная работа 2</summary>
+<img srs="lab2.jpg">
+
+<summary>Лабораторная работа 2</summary>
+<img srs="lab3.jpg">
+
+<summary>Лабораторная работа 2</summary>
+<img srs="lab4.jpg">
+
+<summary>Лабораторная работа 2</summary>
+<img srs="lab6.jpg">
+
+</details>
--- lab1/lab1.c
+++ lab1/lab1.c
@@ -6,68 +6,68 @@ int main(int argc, char **argv)
 {
     if (argc != 3) 
     {
-        printf("error\n");
-        printf("invalid number of arguments");
+        printf("Error\n");
+        printf("Invalid number of arguments");
         return 1;
     }
 
-
-        if ((strcmp(argv[1], "-l") == 0) || (strcmp(argv[1], "-c") == 0) || (strcmp(argv[1], "-w") == 0) || (strcmp(argv[1], "--words") == 0) || (strcmp(argv[1], "--bytes") == 0) || (strcmp(argv[1], "--lines") == 0))
+    if ((strcmp(argv[1], "-l") == 0) || (strcmp(argv[1], "-c") == 0) || (strcmp(argv[1], "-w") == 0) ||
+     (strcmp(argv[1], "--words") == 0) || (strcmp(argv[1], "--bytes") == 0) || (strcmp(argv[1], "--lines") == 0))
+    {
+        FILE *fin;
+        fin = fopen(argv[2], "r");
+        int pred_cim, slov = 1, ctr = 1, counter = 0;
+        int cim;
+        cim = fgetc(fin);
+        pred_cim = cim;
+        if (fin != NULL)
         {
-            FILE *fin;
-            fin = fopen(argv[2], "r");
-            int pred_cim, slov = 1, ctr = 1, counter = 0;
-            int cim;
-            cim = fgetc(fin);
-            pred_cim = cim;
-            if (fin != NULL)
+            while (cim != EOF)
             {
-                while (cim != EOF)
+                counter++;
+                if (cim == '\n') 
                 {
-                    counter++;
-                    if (cim == '\n') 
-                    {
-                        ctr++;
-                    }
-                    if (isspace(cim) && (isspace(pred_cim) == 0))
-                    {
-                        slov++;
-                    }
-                    pred_cim = cim;
-                    cim = fgetc(fin);
+                    ctr++;
                 }
-                if (counter == 0)
+                if (isspace(cim) && (isspace(pred_cim) == 0))
                 {
-                    slov = 0;
-                    ctr = 0;
+                    slov++;
                 }
-                fclose(fin);
-                if ((strcmp(argv[1], "-c") == 0) || (strcmp(argv[1], "--bytes") == 0))
-                {
-                    printf("%d\n", counter);
-                }
-                if ((strcmp(argv[1], "-l") == 0) || (strcmp(argv[1], "--lines") == 0))
-                {
-                    printf("%d\n", ctr);
-                }
-                if ((strcmp(argv[1], "-w")==0) || (strcmp(argv[1], "--words")==0))
-                {
-                    printf("%d\n", slov);
-                }
-                
-                return 0;
+                pred_cim = cim;
+                cim = fgetc(fin);
             }
-            else
+            if (counter == 0)
             {
-                printf("error\n");
-                printf("file not found");
-                return 1;
+                slov = 0;
+                ctr = 0;
             }
+            fclose(fin);
+            if ((strcmp(argv[1], "-c") == 0) || (strcmp(argv[1], "--bytes") == 0))
+            {
+                printf("%d\n", counter);
+            }
+            if ((strcmp(argv[1], "-l") == 0) || (strcmp(argv[1], "--lines") == 0))
+            {
+                printf("%d\n", ctr);
+            }
+            if ((strcmp(argv[1], "-w")==0) || (strcmp(argv[1], "--words")==0))
+            {
+                printf("%d\n", slov);
+            }
+            
+            return 0;
         }
         else
         {
             printf("error\n");
-            printf("invalid argument");
+            printf("file not found");
             return 1;
         }
+    }
+    else
+    {
+        printf("Error\n");
+        printf("Invalid argument");
+        return 1;
+    }
 }
